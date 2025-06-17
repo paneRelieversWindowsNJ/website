7e80a1e3dfb123c6c2d5d88f167993db08161020
Trent Green <70169861+Trent325@users.noreply.github.com>
2025-06-17 18:55:38 -0400

    attempt 1 to put on verce;

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vercel from 'vite-plugin-vercel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), vercel()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        Charters: './Charters.html',
+        AboutUs: './Boat.html',
+        Trips: './Trips.html',
+        404: "./404.html",
+        Crew: "./Crew.html"
+      }
+    }
+  },
+ // base: process.env.VITE_BASE_PATH || '/BassLineSportFishingSite',
+ })
