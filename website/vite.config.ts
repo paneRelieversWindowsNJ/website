9a54890b99225bd8453162e28b09218bf6d8fda2
Trent Green <70169861+Trent325@users.noreply.github.com>
2025-07-22 22:12:07 -0400

    fixed vite config

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: './index.html',
-        about: './aboutUS.html',
+        about: './aboutUs.html',
         services: './services.html',
       }
     }
